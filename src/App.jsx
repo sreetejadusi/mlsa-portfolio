4019799d97dee08501041e8107dfa6711b3d1670
Sree Teja Dusi <sreetejadusi@gmail.com>
2023-10-23 19:03:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sree Teja Dusi",
+  title: "Developer and Student",
+  email: "sreetejadusi@gmail.com",
+  gitHub: "sreetejadusi",
+  instagram: "thesreetejadusi",
+  linkedIn: "sreetejadusi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "sreetejadusi",
+  youTube: "sreetejadusi",
 };
 
 const primaryColor = "#4E567E";
